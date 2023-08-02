07dca12ae4189be2b79da5b817c3edea0a73ac3f
j0srisk <j0srisk@gmail.com>
2023-08-02 11:47:06 -0500

    created basic router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div>
-      <h1 className="font-bold">Template React Tailwind</h1>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h1>Hello from the main page of the app!</h1>
+			<p>Here are some examples of links to other pages</p>
+			<nav>
+				<ul>
+					<li>
+						<a href="profile">Profile page</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Profile from './Profile.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/profile',
+		element: <Profile />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 );
