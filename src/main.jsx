cf5978986577578e4f74566a6b4b5970e4a529dd
j0srisk <j0srisk@gmail.com>
2023-08-02 11:54:35 -0500

    testing dynamic segments

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Profile from './Profile.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Profile from "./Profile.jsx";
+import Popeye from "./Popeye.jsx";
+import Spinach from "./Spinach.jsx";
+import DefaultProfile from "./DefaultProfile.jsx";
 
 const router = createBrowserRouter([
 	{
-		path: '/',
+		path: "/",
 		element: <App />,
 	},
 	{
-		path: '/profile',
+		path: "/profile",
 		element: <Profile />,
+		children: [
+			{ index: true, element: <DefaultProfile /> },
+			{ path: "popeye", element: <Popeye /> },
+			{ path: "spinach", element: <Spinach /> },
+		],
 	},
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
 	</React.StrictMode>,
